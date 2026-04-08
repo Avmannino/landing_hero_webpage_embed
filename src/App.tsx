2d99aa0791e4c9cab09165e200d8957479954626
AdamMannino <amannino92@gmail.com>
2026-04-08 11:06:35 -0400

    Initital Commit

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import "./App.css";
+import logo from "./assets/logo.png";
+
+function App() {
+  return (
+    <main className="hero">
+      <div className="hero__background" />
+      <div className="hero__overlay" />
+
+      <section className="hero__content">
+        <div className="hero__inner">
+          <div className="hero__logo-wrap">
+            <img
+              src={logo}
+              alt="Citywide Painting & Design"
+              className="hero__logo"
+            />
+          </div>
+
+          <h1 className="hero__headline">
+            Transforming Commercial Spaces with{" "}
+            <span>Precision &amp; Excellence</span>
+          </h1>
+
+          <div className="hero__actions">
+            <a href="#services" className="hero__button hero__button--filled">
+              Our Services
+            </a>
+
+            <a href="#quote" className="hero__button hero__button--outline">
+              Request a Quote
+            </a>
+          </div>
+
+          <div className="hero__areas">
+            <span>NYC</span>
+            <span className="hero__dot">•</span>
+            <span>WESTCHESTER</span>
+            <span className="hero__dot">•</span>
+            <span>LONG ISLAND</span>
+            <span className="hero__dot">•</span>
+            <span>NORTH JERSEY</span>
+          </div>
+        </div>
+      </section>
+
+      <div id="services" />
+      <div id="quote" />
+    </main>
+  );
+}
+
+export default App;
